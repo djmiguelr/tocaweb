84943bf71d31ebc7e64edbbc21bce8c70f201c3e
djmiguelr <ruedamonsalvemiguel@gmail.com>
2025-04-09 12:13:17 -0500

    Simplificada la configuración de PostCSS

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,25 +1,11 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
     ...(process.env.NODE_ENV === 'production' ? {
-      'cssnano': {
-        preset: ['default', {
-          discardComments: {
-            removeAll: true,
-          },
-          normalizeWhitespace: true,
-        }]
-      },
-      '@fullhuman/postcss-purgecss': {
-        content: [
-          './src/**/*.{js,jsx,ts,tsx}',
-          './public/index.html'
-        ],
-        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-        safelist: ['html', 'body']
+      cssnano: {
+        preset: 'default'
       }
     } : {})
   }
